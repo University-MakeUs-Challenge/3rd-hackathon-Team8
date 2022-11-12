6a8bbcfd6fc0d78dbd78dd1d55ae4569ca691263
lucy <lucy1287@naver.com>
2022-11-13 04:02:04 +0900

    [FEAT] 챌린지 페이지

--- Client/hackathonAndroid/app/src/main/java/com/example/hackatonumc3rd/ChallengeFragment.kt
+++ Client/hackathonAndroid/app/src/main/java/com/example/hackatonumc3rd/ChallengeFragment.kt
@@ -8,11 +8,18 @@ import androidx.fragment.app.Fragment
 import com.example.hackatonumc3rd.databinding.FragmentChallengeBinding
 
 class ChallengeFragment: Fragment() {
+    private lateinit var viewBinding: FragmentChallengeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        viewBinding = FragmentChallengeBinding.inflate(layoutInflater)
+
+
+
+
         return FragmentChallengeBinding.inflate(layoutInflater).root
     }
 }
