2c13971cdfb921fb28c45b7edbec9ca42745a886
ChaeYubin <63189595+ChaeYubin@users.noreply.github.com>
2022-11-13 01:29:44 +0900

    [FEAT] 마이페이지 프래그먼트 구성 완료

--- Client/hackathonAndroid/app/src/main/java/com/example/hackatonumc3rd/MypageFragment.kt
+++ Client/hackathonAndroid/app/src/main/java/com/example/hackatonumc3rd/MypageFragment.kt
@@ -1,17 +1,42 @@
 package com.example.hackatonumc3rd
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.hackatonumc3rd.databinding.FragmentHomeBinding
 import com.example.hackatonumc3rd.databinding.FragmentMypageBinding
 
 class MypageFragment: Fragment() {
+    private lateinit var viewBinding: FragmentMypageBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return FragmentMypageBinding.inflate(layoutInflater).root
+        viewBinding = FragmentMypageBinding.inflate(layoutInflater)
+
+        val ssb = SpannableStringBuilder("10승 | 3패")
+        ssb.apply {
+            setSpan(ForegroundColorSpan(Color.BLUE), 0, 3, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            setSpan(ForegroundColorSpan(Color.RED), 6, 8, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+
+        viewBinding.tvWinlose.text = ssb
+
+        viewBinding.btnEditProfile.setOnClickListener {
+            /* 프로필 수정 버튼 */
+        }
+
+        viewBinding.btnBadgeCf.setOnClickListener {
+            /* 전체 뱃지 확인 버튼 */
+        }
+
+
+        return viewBinding.root
     }
 }
