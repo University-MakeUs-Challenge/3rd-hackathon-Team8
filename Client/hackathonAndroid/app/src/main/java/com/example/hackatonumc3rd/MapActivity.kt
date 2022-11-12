35b1130253ec7b532ccaeeac1de8a86f59874e1c
ChaeYubin <63189595+ChaeYubin@users.noreply.github.com>
2022-11-12 23:51:54 +0900

    [FEAT] #3 - kakao map marker, cirle

--- Client/hackathonAndroid/app/src/main/java/com/example/hackatonumc3rd/MapActivity.kt
+++ Client/hackathonAndroid/app/src/main/java/com/example/hackatonumc3rd/MapActivity.kt
@@ -1,8 +1,12 @@
 package com.example.hackatonumc3rd
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hackatonumc3rd.databinding.ActivityMapBinding
+import net.daum.mf.map.api.MapCircle
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
 
 class MapActivity : AppCompatActivity() {
@@ -13,6 +17,20 @@ class MapActivity : AppCompatActivity() {
         setContentView(viewBinding.root)
 
         val mapView = MapView(this)
+        val marker = MapPOIItem()
+        val markerPoint = MapPoint.mapPointWithGeoCoord(37.49, 127.03)
+
+        marker.itemName = "Me"
+        marker.tag = 0
+        marker.mapPoint = markerPoint
+        marker.markerType = MapPOIItem.MarkerType.RedPin
+
+        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(37.49, 127.03), 3, true)
+        mapView.addPOIItem(marker)
+
+        val circle = MapCircle(MapPoint.mapPointWithGeoCoord(37.49, 127.03), 500, Color.argb(100, 255, 0, 0), Color.argb(85, 0, 255, 0))
+        circle.tag = 1234
+        mapView.addCircle(circle)
         viewBinding.mapView.addView(mapView)
     }
 }
